266ec7906a6583917ac7004428e5f16cab2b327e
paul <paul@bizthinking.com.tw>
2023-08-06 11:34:16 +0800

    first commit

---
+++ WeatherWebApplication/WeatherWebApplication/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.FeatureManagement;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddFeatureManagement();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast()
+        {
+            DateTime = DateOnly.FromDateTime((DateTime.Now).AddDays(index)),
+            Temperature = Random.Shared.Next(-20, 55),
+            Status = summaries[Random.Shared.Next(summaries.Length)]
+        }).ToArray();
+        return forecast;
+    }).WithName("GetWetherForecast")
+    .WithOpenApi();
+app.Run();
+
+public class WeatherForecast
+{
+    public DateOnly DateTime { get; set; }
+    public int Temperature { get; set; }
+    public string Status { get; set; }
+}
